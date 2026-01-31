89ad46994a33bcdff2c6324a6df9bc8bc6fc96b6
zenisis <zenisis@example>
2026-01-31 12:11:08 +0530

    added the app.service changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '********************************* Welcome To kartik-portfolio *********************************';
+    return '********************************* Welcome to my portfolio *********************************';
   }
 }
